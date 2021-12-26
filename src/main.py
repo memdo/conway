f14dd44a609faeeaa992f43bc1381ec3563a6895
M. Berkay Düzenli <memdo@pm.me>
2021-12-26 22:35:37 +0300

    Randomize items of the matrix

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import random
+
 def dead_state(width, height):
     board = []
     for _ in range(height):
@@ -6,4 +8,11 @@ def dead_state(width, height):
             temp.append(0)
         board.append(temp)
     return board
+
+def random_state(width, height):
+    state = dead_state(width, height)
+    for h in range(height):
+        for w in range(width):
+            state[h][w] = random.choice((0, 1))
+    return state
 
